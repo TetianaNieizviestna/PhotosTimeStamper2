20f3e54fa6d2bcc3b8e2283a777288b82028134e
Тетяна Нєізвєстна <tatiana.neizvestnaya@onix-systems.com>
2021-12-17 09:50:53 +0200

    add pods

---
+++ PhotosTimeStamper/Pods/Target Support Files/Pods-PhotosTimeStamper-PhotosTimeStamperUITests/Pods-PhotosTimeStamper-PhotosTimeStamperUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotosTimeStamper_PhotosTimeStamperUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PhotosTimeStamper_PhotosTimeStamperUITests
+@end
